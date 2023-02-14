0ad655cb27c3bd7115ef34f238d37952bcbd8b2d
alalkaorh <alalkaorh@ivcloud.com>
2023-02-14 11:38:56 +0700

    сделала до 10 части без практических

--- js/main.js
+++ js/main.js
@@ -1,10 +1,99 @@
-let product = "Socks";
+ Vue.component('product', {
+    template: `
+    <div class="product">
+        <div class="product-image">
+            <img :src="image" :alt="altText"/>
+        </div>
+    
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p v-if="inStock">In stock</p>
+            <p v-else>Out of Stock</p>
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+         </ul>
+         
+            
+            
+    
+            <div
+                    class="color-box"
+                    v-for="(variant, index) in variants"
+                    :key="variant.variantId"
+                    :style="{ backgroundColor:variant.variantColor }"
+                    @mouseover="updateProduct(index)"
+            ></div>
+            <p>Доставка: {{ shipping }}</p>
+            <div class="cart">
+                <p>Cart({{ cart }})</p>
+            </div>
+    
+            <button
+                    v-on:click="addToCart"
+                    :disabled="!inStock"
+                    :class="{ disabledButton: !inStock }"
+            >
+                Add to cart
+            </button>
+        </div>
+    </div>
+ `,
+    data() {
+        return {
+            product: "Socks",
+            selectedVariant: 0,
+            altText: "A pair of socks",
+            brand: 'Vue Mastery',
+            OnSale: true,
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [{variantId: 2234, variantColor: 'green', variantImage: "./assets/vmSocks-green-onWhite.jpg", variantQuantity: 15}, 
+                        {variantId: 2235, variantColor: 'blue', variantImage: "./assets/vmSocks-blue-onWhite.jpg", variantQuantity: 5}],
+            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            cart: 1,
+        }
+    },
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+        methods: {
+            addToCart() {
+                this.cart += 1
+            },
+            updateProduct(index) {
+                this.selectedVariant = index;
+                console.log(index);
+            }
+        },
+        computed: {
+            title() {
+                return this.brand + ' ' + this.product;
+            },
+            image() {
+                return this.variants[this.selectedVariant].variantImage;
+            },
+            inStock(){
+                return this.variants[this.selectedVariant].variantQuantity
+            },
+            shipping() { 
+                if (this.premium) {
+                    return "бесплатно";
+                } else {
+                    return 2.99
+                }
+            }
+        }
 
-let app = new Vue({
+ })
+
+ let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        premium: true
     }
-})
+ })
+ 
 
-
+ 
